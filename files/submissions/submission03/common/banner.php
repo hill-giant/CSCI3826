dd29843dee707e07e3d681d96ff96459c7e28282
hill-giant <nathanclowes@gmail.com>
2020-03-08 14:35:16 -0300

    Fix validation errors

--- files/submissions/submission03/common/banner.php
+++ files/submissions/submission03/common/banner.php
@@ -1,7 +1,7 @@
       <div class="w3-border w3-border-black w3-yellow">
         <div id="logo" class="w3-half w3-center">
           <img src="images/logo.png" alt="JaCoT Logo"
-            style="width=100%">
+            style="width: 100%">
         </div>
         <div class="w3-half w3-right-align">
           <div class="w3-panel">
@@ -12,4 +12,5 @@
             ?>
           </div>
         </div>
-        <script src="scripts/datetime.js"></script>
+      </div>
+      <script src="scripts/datetime.js"></script>
